5655f2e3ea8f2b5ac00ece392702eb188daff600
zoomlyly <ty160518@qq.com>
2015-12-31 13:03:41 +0800

    修改匹配

--- modules/layer.js
+++ modules/layer.js
@@ -8,6 +8,29 @@ module.exports = (function() {
         dependencies: [
             "jquery@>=1.8"
         ],
+        mapping: [
+            {
+                reg: '/extend/layer.ext.js',
+                release: '$&'
+            },
+            {
+                reg: '/skin/*.css',
+                release: '$&'
+            },
+            {
+                reg: /^\/skin\/default\/([^\/]*\.(png|gif))$/,
+                release: '$&'
+            },
+            
+            {
+                reg: /^\/README\.md$/,
+                release: '$&'
+            },
+            {
+                reg: '*',
+                release: false
+            }
+        ],
         shim: {
             "layer.js": {
               "deps": ["jquery"]
